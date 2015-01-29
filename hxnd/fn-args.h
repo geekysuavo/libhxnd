8d16529352919adaabc7c7807b7d69e4db7ffaef
geekysuavo <geekysuavo@gmail.com>
2015-01-29 14:53:39 -0600

    added function argument definitions header

---
+++ hxnd/fn-args.h
@@ -0,0 +1,107 @@
+
+/* hxnd: A framework for n-dimensional hypercomplex calculations for NMR.
+ * Copyright (C) 2014  Bradley Worley  <geekysuavo@gmail.com>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to:
+ *
+ *   Free Software Foundation, Inc.
+ *   51 Franklin Street, Fifth Floor
+ *   Boston, MA  02110-1301, USA.
+ */
+
+/* ensure once-only inclusion. */
+#ifndef __HXND_FN_ARGS_H__
+#define __HXND_FN_ARGS_H__
+
+/* include the processing function header. */
+#include <hxnd/fn.h>
+
+static fn_arg fn_args_add[] = {
+  { "const",    { .f = 0.0  }, FN_VALTYPE_FLOAT },
+  { "file",     { .s = NULL }, FN_VALTYPE_STRING },
+  { "scale",    { .f = 1.0  }, FN_VALTYPE_FLOAT },
+  { "subtract", { .b = 0    }, FN_VALTYPE_BOOL },
+  { NULL,       {},            FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_cut[] = {
+  { "trace", { .iv = NULL }, FN_VALTYPE_INTS },
+  { "plane", { .iv = NULL }, FN_VALTYPE_INTS },
+  { NULL,    {},             FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_fft[] = {
+  { "alternate", { .b = 0 }, FN_VALTYPE_BOOL },
+  { "negate",    { .b = 0 }, FN_VALTYPE_BOOL },
+  { "inverse",   { .b = 0 }, FN_VALTYPE_BOOL },
+  { NULL,        {},         FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_ist[] = {
+  { "thresh", { .f = 0.9 }, FN_VALTYPE_FLOAT },
+  { "iters",  { .i = 200 }, FN_VALTYPE_INT },
+  { NULL,     {},           FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_phase[] = {
+  { "ph0",     { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { "ph1",     { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { "pivot",   { .f = 0.5 }, FN_VALTYPE_FLOAT },
+  { "ppm",     { .b = 0   }, FN_VALTYPE_BOOL },
+  { "hz",      { .b = 0   }, FN_VALTYPE_BOOL },
+  { "inverse", { .b = 0   }, FN_VALTYPE_BOOL },
+  { NULL,      {},           FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_resize[] = {
+  { "size",  { .i = 0     }, FN_VALTYPE_INT },
+  { "shape", { .iv = NULL }, FN_VALTYPE_INTS },
+  { NULL,    {},             FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_scale[] = {
+  { "first",  { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { "factor", { .f = 1.0 }, FN_VALTYPE_FLOAT },
+  { "invert", { .b = 0   }, FN_VALTYPE_BOOL },
+  { NULL,     {},           FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_shift[] = {
+  { "points", { .b = 0   }, FN_VALTYPE_BOOL },
+  { "sec",    { .b = 0   }, FN_VALTYPE_BOOL },
+  { "ppm",    { .b = 0   }, FN_VALTYPE_BOOL },
+  { "hz",     { .b = 0   }, FN_VALTYPE_BOOL },
+  { "round",  { .b = 0   }, FN_VALTYPE_BOOL },
+  { "amount", { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { NULL,     {},           FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_window[] = {
+  { "type", { .s = NULL }, FN_VALTYPE_STRING },
+  { "start", { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { "end",   { .f = 1.0 }, FN_VALTYPE_FLOAT },
+  { "order", { .f = 1.0 }, FN_VALTYPE_FLOAT },
+  { "lb",     { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { "invlb",  { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { "center", { .f = 0.0 }, FN_VALTYPE_FLOAT },
+  { NULL, {}, FN_VALTYPE_UNKNOWN }
+};
+
+static fn_arg fn_args_zerofill[] = {
+  { "times", { .i = 0 }, FN_VALTYPE_INT },
+  { NULL,    {},         FN_VALTYPE_UNKNOWN }
+};
+
+#endif /* __HXND_FN_ARGS_H__ */
+
